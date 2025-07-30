c31bac281c6e94b450bbd1d776c7b08bcdf0f77d
aaroncox <aaron.cox@greymass.com>
2025-07-29 18:11:18 -0700

    Dynamically load contracts on startup instead of through codegen

--- src/lib/wharf/contracts.ts
+++ src/lib/wharf/contracts.ts
@@ -1,20 +1,30 @@
-import { Contract as NoopContract } from '../contracts/noop';
-import { Contract as SystemContract } from '../contracts/system';
-import { Contract as TokenContract } from '../contracts/token';
+import { ContractKit } from '@wharfkit/contract';
 
+import { generalLog } from '$lib/logger';
 import { client } from '$lib/wharf/client';
 import {
+	ANTELOPE_NODEOS_API,
 	ANTELOPE_NOOP_CONTRACT,
 	ANTELOPE_SYSTEM_CONTRACT,
 	ANTELOPE_TOKEN_CONTRACT
 } from 'src/config';
 
-export const noopContract = new NoopContract({ client, account: ANTELOPE_NOOP_CONTRACT });
-export const systemContract = new SystemContract({
-	client,
-	account: ANTELOPE_SYSTEM_CONTRACT
+const kit = new ContractKit({ client });
+
+export const noopContract = await kit.load(ANTELOPE_NOOP_CONTRACT);
+generalLog.info(`Loaded noop contract`, {
+	contract: ANTELOPE_NOOP_CONTRACT,
+	url: ANTELOPE_NODEOS_API
 });
-export const tokenContract = new TokenContract({
-	client,
-	account: ANTELOPE_TOKEN_CONTRACT
+
+export const systemContract = await kit.load(ANTELOPE_SYSTEM_CONTRACT);
+generalLog.info(`Loaded system contract`, {
+	contract: ANTELOPE_SYSTEM_CONTRACT,
+	url: ANTELOPE_NODEOS_API
+});
+
+export const tokenContract = await kit.load(ANTELOPE_TOKEN_CONTRACT);
+generalLog.info(`Loaded token contract`, {
+	contract: ANTELOPE_TOKEN_CONTRACT,
+	url: ANTELOPE_NODEOS_API
 });
