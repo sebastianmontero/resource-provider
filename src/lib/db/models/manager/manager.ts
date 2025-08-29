80f058add723727a03dc4f88f210204ecc138fb0
Sebastian Montero <sebastian.montero@gmail.com>
2025-08-29 15:46:44 -0600

    Removed the storing of the manager account from the database, only the env vars are used

--- src/lib/db/models/manager/manager.ts
+++ src/lib/db/models/manager/manager.ts
@@ -13,37 +13,11 @@ import {
 
 export class ManagerAccountDatabase extends AbstractDatabase {
 	async getManagerAccount() {
-		const results = await database
-			.select()
-			.from(this.schema.manager)
-			.where(eq(this.schema.manager.account, MANAGER_ACCOUNT_NAME))
-			.limit(1)
-			.catch((error) => {
-				managerLog.error('Error fetching manager account from database', { error });
-				throw error;
-			});
-		const manager = results[0];
-		const key = MANAGER_ACCOUNT_PRIVATEKEY || String(PrivateKey.generate('K1'));
-		if (!manager) {
-			const values = {
-				account: MANAGER_ACCOUNT_NAME,
-				permission: MANAGER_ACCOUNT_PERMISSION || 'manager',
-				key
-			};
-			managerLog.debug(
-				'Creating database entry for manager account from environment variables.',
-				objectify({
-					...values,
-					key: PrivateKey.from(key).toPublic()
-				})
-			);
-			await database.insert(this.schema.manager).values(values).onConflictDoUpdate({
-				target: this.schema.manager.account,
-				set: values
-			});
-			return values;
-		}
-		return manager;
+		return {
+			account: MANAGER_ACCOUNT_NAME,
+			permission: MANAGER_ACCOUNT_PERMISSION || 'manager',
+			key: MANAGER_ACCOUNT_PRIVATEKEY
+		};
 	}
 }
 
